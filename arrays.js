91fabf68fe41b8b7d1a720e01c1f4a02d3e3a4bf
Ariel Ross <arielross92@gmail.com>
2017-12-28 03:03:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array,index){
 }
 
 function removeElementFromBeginningOfArray(array){
-  
+  return array.slice(-1)
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
