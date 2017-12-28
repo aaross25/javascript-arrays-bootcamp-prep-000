d19196df162194538bff2ceef7f58abcc263d81e
Ariel Ross <arielross92@gmail.com>
2017-12-28 02:49:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,8 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  array.push(element)
-  return array
+ return array.push(element)
   
 }
 
